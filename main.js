fcfdeb0f1134f765c8a2804e19151176edf9ee84
joaovitorquaglio <103510756+joaovitorquaglio@users.noreply.github.com>
2024-10-28 11:48:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function showMore() {
+    window.scrollTo({
+    top: document.getElementById("features").offsetTop,
+    behavior: "smooth"
+    });
+    }
+    
+    function buyNow() {
+    alert("Obrigado pelo seu interesse! Em breve estará disponível para compra.");
+    }
+    
+    
